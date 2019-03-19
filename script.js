6a9f2197696a940303a2e49193c71ee30e42105f
Andrey <alexandrov_andrey@yahoo.com>
2019-03-19 12:31:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+var images = ["thumbnail.jpg", "book.jpg", "thumbnail.jpg", "book.jpg", "banana.jpg", "palm.jpg"];
+var descriptions = ["space+trees"];
+
+console.log("hello");
+
+$(document).ready( function () {
+
+});
